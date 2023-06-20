4ee1139bcd8b592615e21fb8b0a8daf4cb9b7840
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-06-20 15:46:21 +0700

    update: xe_tot crawler

--- crawlers/xe_tot.py
+++ crawlers/xe_tot.py
@@ -6,45 +6,59 @@ from utils import *
 
 DATA_PATH = "data/xe_tot"
 PATH_TO_LINKS = DATA_PATH + "/links.csv"
-PATH_TO_CARS = DATA_PATH + "cars.csv"
+PATH_TO_CARS = DATA_PATH + "/raw_cars.csv"
+BASE_URL = "https://xetot.com"
 
 
 class XeTotCrawler(Crawler):
     def crawl(self):
         """crawl cars data and save to csv file"""
-        cars_df = create_empty_cars_df()
-        links_df = self.get_links()
+        links = self.get_links()[:1]
+        cars = []
 
-        for index, row in links_df.iterrows():
-            link = row["link"]
-            car_df = self.get_car_df(link)
-            cars_df = pd.concat([cars_df, car_df], ignore_index=True)
+        for link in links:
+            res = requests.get(link).text
+            soup = BeautifulSoup(res, "html.parser")
+            tableSelector = ".product-info > .list-unstyled"
+            liTags = soup.select(tableSelector)[0].find_all('li')
+            car = {}
+            for liTag in liTags:
+                feature = liTag.find_all('div', class_="info-label")[0].text.strip()
+                value = liTag.find_all('div', class_="info-show")[0].text.strip()
+                car[feature] = value
+            cars.append(car)
 
+        cars_df = pd.DataFrame.from_records(cars)
         cars_df.to_csv(PATH_TO_CARS)
 
     def get_links(self):
-        """
-        Returns:
-            DataFrame: a DataFrame with at least a "link" column
-        """
         try:
             links_df = pd.read_csv(PATH_TO_LINKS)
-            return links_df
+            links = links_df[0].values.tolist()
+            if links:
+                return links
         except:
             pass
 
-        # TODO: implement logic for getting links to car details
-        links = {
-            "links": [],
-        }
+        links = []
+
+        page_urls = [
+            "https://xetot.com/toan-quoc/mua-ban-oto?page=" + str(i)
+            for i in range(1, 2) # TODO: change range to 1, 100 depend on page data
+        ]
+
+        for page_url in page_urls:
+            res = requests.get(page_url).text
+            soup = BeautifulSoup(res, "html.parser")
+            ulSelector = ".list-archive-item > ul"
+            ul = soup.select(ulSelector)[0]
+            aTags = ul.find_all("a", class_="item-title")
+
+            links += [BASE_URL + a['href'] for a in aTags]
+
         links_df = pd.DataFrame(links)
 
         # write data to file so we don't have to get links again
         links_df.to_csv(PATH_TO_LINKS)
 
-        return links_df
-
-    def get_car_df(self, link):
-        # TODO: implement logic for extracting car features from a link
-        brand = "Example Brand"
-        return create_car_df_from_features(brand=brand)
+        return links
